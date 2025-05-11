d11d97bd1638ed8a9c917c2a6472dac223cd5607
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-05-11 23:46:52 +0530

    Fouth

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,9 +1,10 @@
 import React,{useState,useEffect} from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const UserList  = ()=>{
 
     const [names,setNames] = useState([])
+    const navigate = useNavigate()
 
     useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/users")
@@ -11,13 +12,23 @@ const UserList  = ()=>{
         .then(data=>setNames(data))
     },[])
 
+    function handleClick(id){
+        navigate(`/users/${id}`)
+    }
+
     return(
         <ul>
             <h1>User List</h1>
             {
                 names.map(name=>{
                    return <li key={name.id}>
-                            <a href = {`/users/${name.id}`}> {name.name}</a>
+                            <a href = {`/users/${name.id}`} 
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    handleClick(name.id);
+                            }}> 
+                            {name.name}
+                            </a>
                         </li>
                 })
             }
