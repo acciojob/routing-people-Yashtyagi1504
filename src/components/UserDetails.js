b62f23aec0a515fc68ca040ceeb1c6d8a2a0ebfd
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-05-12 00:42:49 +0530

    mmmmm

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,29 +1,36 @@
-import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const UserDetails = ()=>{
-    const [user,setUser] = useState({})
-    const [loading,setLoading] = useState(false)
-    const {id} = useParams()
-    useEffect(()=>{
-        setLoading(true)
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=>res.json()).then(data=>{setUser(data) 
-            setLoading(false)})
-    },[id])
-    return(
+const UserDetails = () => {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTimeout(() => {
+          setUser(data);
+          setLoading(false);
+        }, 300);
+      });
+  }, [id]);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
         <div>
-            {
-                loading ? <div>Loading...</div>  : <div>
-            <p>Name: {user.name}</p>
-            <p>Username: {user.username}</p>
-            <p>Email: {user.email}</p>
-            <p>Phone: {user.phone}</p>
-            <p>Website: {user.website}</p>
+          <p>Name: {user.name}</p>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Website: {user.website}</p>
         </div>
-            }
-        </div>
-        
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default UserDetails
+export default UserDetails;
